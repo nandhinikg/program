43926dff11385eab4842f3cb9df7f643a780a7d1
nandhinikg <135498569+nandhinikg@users.noreply.github.com>
2023-06-04 10:51:23 +0400

    operator

---
+++ Condition.java
@@ -0,0 +1,12 @@
+package org.condition;
+
+public class Condition {
+	public static void main(String[] args) {
+		for (int i = 3; i >=1; i--) {
+			for (int j = 3; j >=0; j--) {
+				System.out.print(5);	}
+			System.out.println();
+		}
+	}
+
+}
